7a506731aa2d7176c844bc1a3f5f6836d4b6763f
tapishgrover <122139964+tapishgrover@users.noreply.github.com>
2026-02-15 04:39:31 -0500

    Create AudioProcessor.swift

---
+++ flo/Shared/Services/AudioProcessor.swift
@@ -0,0 +1,110 @@
+//
+//  AudioProcessor.swift
+//  flo
+//
+//  Created by [Your Name] on [Date].
+//
+
+import AVFoundation
+import Combine
+
+class AudioProcessor: ObservableObject {
+    static let shared = AudioProcessor()
+    
+    private let engine = AVAudioEngine()
+    private let playerNode = AVAudioPlayerNode()
+    private let eq = AVAudioUnitEQ(numberOfBands: 6)
+    private let limiter = AVAudioUnitPeakLimiter()
+    
+    @Published var eqBands: [Float] = [0, 0, 0, 0, 0, 0] {
+        didSet { applyEQ() }
+    }
+    
+    @Published var limiterAttack: Float = 1.0 {
+        didSet { limiter.attackTime = limiterAttack / 1000.0 }
+    }
+    @Published var limiterRelease: Float = 60.0 {
+        didSet { limiter.releaseTime = limiterRelease / 1000.0 }
+    }
+    @Published var limiterPreGain: Float = 0.0 {
+        didSet { limiter.preGain = limiterPreGain }
+    }
+    
+    private init() {
+        setupEQ()
+        setupLimiter()
+        setupEngine()
+        loadSettings()
+    }
+    
+    private func setupEQ() {
+        let frequencies: [Float] = [54, 148, 403, 1096, 2980, 8103]
+        for (index, freq) in frequencies.enumerated() {
+            eq.bands[index].frequency = freq
+            eq.bands[index].bandwidth = 0.5
+            eq.bands[index].gain = 0.0
+            eq.bands[index].bypass = false
+        }
+    }
+    
+    private func setupLimiter() {
+        limiter.attackTime = limiterAttack / 1000.0
+        limiter.releaseTime = limiterRelease / 1000.0
+        limiter.preGain = limiterPreGain
+    }
+    
+    private func setupEngine() {
+        engine.attach(playerNode)
+        engine.attach(eq)
+        engine.attach(limiter)
+        
+        engine.connect(playerNode, to: eq, format: nil)
+        engine.connect(eq, to: limiter, format: nil)
+        engine.connect(limiter, to: engine.mainMixerNode, format: nil)
+        
+        try? engine.start()
+    }
+    
+    func play(url: URL) {
+        playerNode.stop()
+        
+        guard let file = try? AVAudioFile(forReading: url) else { return }
+        playerNode.scheduleFile(file, at: nil)
+        playerNode.play()
+    }
+    
+    func pause() {
+        playerNode.pause()
+    }
+    
+    func resume() {
+        playerNode.play()
+    }
+    
+    func stop() {
+        playerNode.stop()
+    }
+    
+    private func applyEQ() {
+        for (index, gain) in eqBands.enumerated() {
+            eq.bands[index].gain = gain
+        }
+    }
+    
+    // MARK: - Persistence
+    private func loadSettings() {
+        if let saved = UserDefaults.standard.array(forKey: "eqBands") as? [Float] {
+            eqBands = saved
+        }
+        limiterAttack = UserDefaults.standard.float(forKey: "limiterAttack")
+        limiterRelease = UserDefaults.standard.float(forKey: "limiterRelease")
+        limiterPreGain = UserDefaults.standard.float(forKey: "limiterPreGain")
+    }
+    
+    func saveSettings() {
+        UserDefaults.standard.set(eqBands, forKey: "eqBands")
+        UserDefaults.standard.set(limiterAttack, forKey: "limiterAttack")
+        UserDefaults.standard.set(limiterRelease, forKey: "limiterRelease")
+        UserDefaults.standard.set(limiterPreGain, forKey: "limiterPreGain")
+    }
+}
